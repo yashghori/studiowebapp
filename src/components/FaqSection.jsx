6afe4701a682d2cec6e3772898bd6d3a3f41145b
yashghori <yashghori2804@gmail.com>
2022-05-25 13:55:50 +0530

    About page UI ready

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+import { About } from '../Style'
 
 const FaqSection = () => {
     return (
         <div>
-            <div className="faq">
-                <h2>Any  Questions <span>FAQ</span> </h2>
+            <FaqStyle>
+                <h2>Any  Questions ?<span>FAQ</span> </h2>
                 <div className="question">
                     <h4>How do we start ?</h4>
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, quae.</p>
                     </div>
+                    <div className="faq_line"></div>
                 </div>
                 <div className="question">
                     <h4>Daily schdule</h4>
@@ -18,6 +21,7 @@ const FaqSection = () => {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, quae.</p>
                     </div>
+                    <div className="faq_line"></div>
                 </div>
                 <div className="question">
                     <h4>How do we accept payment ?</h4>
@@ -25,6 +29,7 @@ const FaqSection = () => {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, quae.</p>
                     </div>
+                    <div className="faq_line"></div>
                 </div>
                 <div className="question">
                     <h4>What product do we offer </h4>
@@ -32,11 +37,40 @@ const FaqSection = () => {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, quae.</p>
                     </div>
+                    <div className="faq_line"></div>
                 </div>
-         
-            </div>
+
+            </FaqStyle>
         </div>
     )
 }
+
+const FaqStyle = styled(About)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .question{
+        cursor: pointer;
+        padding: 1rem 0;
+    }
+    .answer{
+        padding : 2rem 0;
+        p{
+            padding: 1rem 0;
+        }
+    }
+    .faq_line{
+        background: #cccccc;
+        height:0.2rem;
+        margin : 2rem 0;
+        width: 100%;
+    }
+
+`
 
 export default FaqSection
