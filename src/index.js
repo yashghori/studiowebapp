16d4ec4b192d1291f0a38ba851743a8b1e86ed06
yashghori <yashghori2804@gmail.com>
2022-05-26 22:10:43 +0530

    add hashrouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
